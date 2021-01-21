9b6901ceea6466e333b4fcedc7fbefaa4a2def1f
Jordan Machita <jmachita03@aol.com>
2021-01-21 17:44:15 -0500

    updated recusrion for determinant

--- Linear.py
+++ Linear.py
@@ -46,30 +46,30 @@ def multiply(a, b):
         i += 1
     return ab 
 
-def det2(matrix):
-    if matrix.shape == (2,2): # ensures that this is a 2x2 square matrix
-        # assigns each position in the matrix to a variable
-        a = matrix.loc[0][0] 
-        b = matrix.loc[0][1]
-        c = matrix.loc[1][0]
-        d = matrix.loc[1][1]
-        return a*d - b*c # calculates the determinant based on the formula for the determinant of a 2x2 matrix
-    else:
-        print('Not a 2x2 matrix.')
-
-def det3(mat):
-    if mat.shape != (3,3): # ensures that the matrix given is a 3x3
-        print('Not a 3x3 matrix.')
+def determinant(mat):
+    if mat.shape[0] != mat.shape[1]:
+        print("Error: This is not a square matrix.")
         return None
+
+    # calculation for 2x2 matrix
+    if mat.shape == (2,2):
+        r = mat.shape[0]
+        a = mat.loc[r-2][r-2]
+        b = mat.loc[r-2][r-1]
+        c = mat.loc[r-1][r-2]
+        d = mat.loc[r-1][r-1]
+        return a*d - b*c
+    
     cos = list(mat.loc[0]) # creates a list that is the first row of the given matrix
     cofs = []
     inc = 0
-    for item in cos: # creates a list that is cos but with every other term being the opposite sign of the original
+    for item in cos: # replicates cos to cofs with every other sign being opposite
         if inc % 2 == 0:
             cofs.append(item)
         if inc % 2 == 1:
             cofs.append(-item)
         inc += 1
+        
     d = []
     count2 = 0
     for a in cofs:
@@ -84,138 +84,18 @@ def det3(mat):
     d2 = []
     for item in d:
         m2 = pd.DataFrame()
-        m2['Col1'] = item[0]
-        m2['Col2'] = item[1]
-        d2.append(det2(m2)) # gets the determinant of each matrix corresponding to each cofactor
+        num = 0
+        for col in item:
+            name = 'Col' + str(num)
+            m2[name] = item[num]
+            num += 1
+        if m2.shape[0] >= 2: # gets the determinant of each matrix corresponding to each cofactor
+            d2.append(determinant(m2))
+        else:
+            print("Error.")
     final_det = 0
     count3 = 0
     for item in cofs: # multiplies each cofactor in cofs by the coressponding determinant and adds them all
         final_det += item*d2[count3]
-        count3 += 1
-    return final_det 
-
-# Note: The logic for the determinant of a 3x3 is the same as 4x4, 5x5, etc.
-# When calculating a 3x3, the determinants of three 2x2s must be calculated using the det2 function
-# When calculating a 4x4, the determinants of four 3x3s must be calculated using the det3 function
-# And so on
-# Thus, the det6 function uses the det5 function 5 times, which uses the det4 function 4 times, which uses the det3 program 3 times, which uses the det2 program 2 times
-# After a 3x3, the code needs only minor additons to get to det4, so all of the logic is the same and the code is scalable
-
-
-def det4(mat):
-    if mat.shape != (4, 4):
-        print('Not a 4x4 matrix.')
-        return None
-    cos = list(mat.loc[0])
-    cofs = []
-    inc = 0
-    for item in cos:
-        if inc % 2 == 0:
-            cofs.append(item)
-        if inc % 2 == 1:
-            cofs.append(-item)
-        inc += 1
-    d = []
-    count2 = 0
-    for a in cofs:
-        dets = []
-        count = 0
-        while count < mat.shape[0]:
-            if count != count2:
-                dets.append(list(mat.iloc[1:,count]))
-            count += 1
-        d.append(dets)
-        count2 += 1
-    d3 = []
-    for item in d:
-        m2 = pd.DataFrame()
-        m2['Col1'] = item[0]
-        m2['Col2'] = item[1]
-        m2['Col3'] = item[2]
-        d3.append(det3(m2))
-    final_det = 0
-    count3 = 0
-    for item in cofs:
-        final_det += item*d3[count3]
-        count3 += 1
-    return final_det
-
-
-def det5(mat):
-    if mat.shape != (5, 5):
-        print('Not a 5x5 matrix.')
-        return None
-    cos = list(mat.loc[0])
-    cofs = []
-    inc = 0
-    for item in cos:
-        if inc % 2 == 0:
-            cofs.append(item)
-        if inc % 2 == 1:
-            cofs.append(-item)
-        inc += 1
-    d = []
-    count2 = 0
-    for a in cofs:
-        dets = []
-        count = 0
-        while count < mat.shape[0]:
-            if count != count2:
-                dets.append(list(mat.iloc[1:,count]))
-            count += 1
-        d.append(dets)
-        count2 += 1
-    d4 = []
-    for item in d:
-        m2 = pd.DataFrame()
-        m2['Col1'] = item[0]
-        m2['Col2'] = item[1]
-        m2['Col3'] = item[2]
-        m2['Col4'] = item[3]
-        d4.append(det4(m2))
-    final_det = 0
-    count3 = 0
-    for item in cofs:
-        final_det += item*d4[count3]
-        count3 += 1
-    return final_det
-
-def det6(mat):
-    if mat.shape != (6, 6):
-        print('Not a 6x6 matrix.')
-        return None
-    cos = list(mat.loc[0])
-    cofs = []
-    inc = 0
-    for item in cos:
-        if inc % 2 == 0:
-            cofs.append(item)
-        if inc % 2 == 1:
-            cofs.append(-item)
-        inc += 1
-    d = []
-    count2 = 0
-    for a in cofs:
-        dets = []
-        count = 0
-        while count < mat.shape[0]:
-            if count != count2:
-                dets.append(list(mat.iloc[1:,count]))
-            count += 1
-        d.append(dets)
-        count2 += 1
-    d5 = []
-    for item in d:
-        m2 = pd.DataFrame()
-        m2['Col1'] = item[0]
-        m2['Col2'] = item[1]
-        m2['Col3'] = item[2]
-        m2['Col4'] = item[3]
-        m2['Col5'] = item[4]
-        d5.append(det5(m2))
-    final_det = 0
-    count3 = 0
-    for item in cofs:
-        final_det += item*d5[count3]
         count3 += 1
     return final_det
